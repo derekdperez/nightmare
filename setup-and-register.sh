4b68930af1aa80a43927c16b2d9b684be35168f2
Derek Perez <derekdperez@gmail.com>
2026-04-14 13:17:45 -0400

    update

--- setup-and-register.sh
+++ setup-and-register.sh
@@ -11,4 +11,4 @@ sleep 10
 API_TOKEN=$(grep COORDINATOR_API_TOKEN deploy/.env | cut -d'=' -f2)
 
 # Register targets
-python3 register_targets.py --server-base-url https://localhost --api-token "$API_TOKEN" --targets-file targets.txt
+python3 register_targets.py --server-base-url http://localhost --api-token "$API_TOKEN" --targets-file targets.txt
