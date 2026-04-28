9daa4cf11a930af630c5a5e1e0299184bec9f904
Derek Perez <derekdperez@gmail.com>
2026-04-27 23:28:44 -0400

    update

--- DotNetSolution/deploy/postgres/init-02-filestore-db.sql
+++
@@ -1,3 +0,0 @@
--- Second database for binary blobs (EF EnsureCreated builds tables on first app start).
--- Runs only on fresh Postgres data volume.
-CREATE DATABASE nightmare_v2_files OWNER nightmare;
